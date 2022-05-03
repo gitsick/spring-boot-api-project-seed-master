6ac918733fb59107e3710bc3a518c09f383a3362
gitsick <zhaoyong@fit2cloud.cn>
2022-05-03 22:03:53 +0800

    更新demo分支更新

--- src/main/java/com/company/project/model/demo.java
+++ src/main/java/com/company/project/model/demo.java
@@ -1,8 +1,14 @@
 package com.company.project.model;
 
+import javax.print.DocFlavor;
+
 /**
  * @author solang
  * @date 2022-05-03 21:29
  */
 public class demo {
+    private String name;
+    private String hobby;
+    private int number;
+    private String phone;
 }
